15e79c00ac520362631facb707a2bba721609732
AriannaToitonova <ariannatoitonova@gmail.com>
2023-06-08 14:15:52 +0600

    dz8

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Parent extends React.Component {
+    render() {
+        return (
+            <div>
+                <Child>
+                    <h1>Заголовок 1</h1>
+                    <p>Текст для дочернего компонента №1</p>
+                </Child>
+                <Child>
+                    <h1>Заголовок 2</h1>
+                    <p>Текст для дочернего компонента №2</p>
+                </Child>
+            </div>
+        );
+    }
 }
 
-export default App;
+class Child extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.children[0]}</h1>
+                <div>{this.props.children[1]}</div>
+            </div>
+        );
+    }
+}
+
+export default Parent;
+
+
+
+
+
+//
+// class Parent extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <Child>
+//                     {({ count, increment, decrement }) => (
+//                         <div>
+//                             <h1>Счетчик: {count}</h1>
+//                             <button
+//                                 onClick={increment}>
+//                                 Увеличить
+//                             </button>
+//                             <button
+//                                 onClick={decrement}>
+//                                 Уменьшить
+//                             </button>
+//                         </div>
+//                     )}
+//                 </Child>
+//             </div>
+//         );
+//     }
+// }
+//
+// class Child extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             count: 0
+//         };
+//     }
+//
+//     increment = () => {
+//         this.setState((prevState) => ({
+//             count: prevState.count + 1
+//         }));
+//     };
+//
+//     decrement = () => {
+//         this.setState((prevState) => ({
+//             count: prevState.count - 1
+//         }));
+//     };
+//
+//     render() {
+//         return (
+//             <div>
+//                 {this.props.children(
+//                     { count: this.state.count, increment: this.increment, decrement: this.decrement })
+//                 }
+//             </div>
+//         );
+//     }
+// }
+//
+// export default Parent;
